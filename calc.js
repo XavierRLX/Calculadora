048e21fd8a265569b2b994c3ba710097b0c2bc00
Renan Xavier <renanlima2000.aer@gmail.com>
2023-04-04 15:46:56 -0300

    Adicionado novo botão de tema e escuro

--- calc.js
+++ calc.js
@@ -22,37 +22,38 @@ function calcular() {
     }
 }
 
-var temax = document.getElementById('mudar-tema')
-var troca = document.getElementById('troca-tema')
-var bts = document.getElementById('igual')
-var todos = document.getElementById('flexbox')
-var sec = document.getElementById('section')
-var xs = document.getElementById('x')
-var re = document.getElementById('res')
-var corp = document.getElementById('corpo')
+var switchBtn = document.getElementById('switch');
+var bts = document.getElementById('igual');
+var todos = document.getElementById('flexbox');
+var sec = document.getElementById('section');
+var xs = document.getElementById('x');
+var re = document.getElementById('res');
+var corp = document.getElementById('corpo');
 
-temax.addEventListener('click', clicar)
-troca.addEventListener('click', trocar)
-
-function clicar(){
-    troca.style.display = 'inline'
-    bts.style.backgroundColor = '#f50d0d'
-    todos.style.backgroundColor = '#3737f3'
-    sec.style.backgroundColor = '#c8c8d8'
-    re.style.backgroundColor = '#c8c8d8'
-    re.style.textDecorationColor = '#c8c8d8'
-    xs.style.textDecorationColor = '#c8c8d8'
-    corp.style.backgroundColor = '#ffffff'
-
+function temaEscuro() {
+  bts.style.backgroundColor = '#f50d0d';
+  todos.style.backgroundColor = '#3737f3';
+  sec.style.backgroundColor = '#c8c8d8';
+  re.style.backgroundColor = '#c8c8d8';
+  re.style.textDecorationColor = '#c8c8d8';
+  xs.style.textDecorationColor = '#c8c8d8';
+  corp.style.backgroundColor = '#ffffff';
 }
-function trocar(){
-    troca.style.display = 'none'
-    bts.style.backgroundColor = '#970808'
-    todos.style.backgroundColor = '#1d1d99'
-    sec.style.backgroundColor = '#464649'
-    re.style.backgroundColor = '#464649'
-    re.style.textDecorationColor = '#464649'
-    xs.style.textDecorationColor = 'red'
-    corp.style.backgroundColor = '#343a4b'
-    
+
+function temaClaro() {
+  bts.style.backgroundColor = '#970808';
+  todos.style.backgroundColor = '#1d1d99';
+  sec.style.backgroundColor = '#464649';
+  re.style.backgroundColor = '#464649';
+  re.style.textDecorationColor = '#464649';
+  xs.style.textDecorationColor = 'red';
+  corp.style.backgroundColor = '#343a4b';
 }
+
+switchBtn.addEventListener("change", function() {
+  if (this.checked) {
+    temaEscuro();
+  } else {
+    temaClaro();
+  }
+});
